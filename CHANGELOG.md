b525c9ef7d0fb43709483e6d37d04d8cdf4f1eff
Kurt Duyck <kurt.duyck@protonmail.com>
2021-03-08 14:17:13 +0100

    Add boilerplate help
    	modified:   CHANGELOG.md
    	modified:   dithering.sh

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.3] - 2021-03-08
+### Changed
+- Add help to dithering sh
+
 ## [0.1.2] - 2021-01-26
 ### Changed
 - Add sample cmd to readme
--- dithering.sh
+++ dithering.sh
@@ -1,4 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t' # The IFS variable - which stands for Internal Field Separator 
 
+usage() {
+  cat <<EOF
+Usage: ./$(basename "$0") [-h] [-V] 
+Bash kitchen sink
+IMG and output is hardcoded in script; 
+
+Options:
+
+-h, --help      Print this help and exit
+-V, --version   Print version
+EOF
+  exit
+}
+
+version() {
+  cat <<EOF
+v0.1.3
+EOF
+  exit 0
+}
+
+parse_params() {
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -V | --version) version ;;
+    *) break ;;
+    esac
+    shift
+  done
+  return 0
+}
+
+parse_params "$@"
+
+echo "-------------------------------"
+echo "       bash kitchen "
+echo "-------------------------------"
 # ~ is not converted when not specifying bin bash?
 SRCDIR='/home/kurt/Pictures/'
 TRTDIR='/home/kurt/MEGA/transferstation/'
